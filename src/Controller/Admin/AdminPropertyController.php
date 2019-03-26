c9dca4edcae7b7492f3e79d96152bd0d53217678
Claire Lozano <claire.lozano@live.fr>
2019-03-26 10:08:14 +0100

    pas fini

---
+++ src/Controller/Admin/AdminPropertyController.php
@@ -0,0 +1,64 @@
+<?php
+namespace App\Controller\Admin;
+
+
+use App\Entity\Property;
+use App\Form\PropertyType;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminPropertyController extends AbstractController {
+
+  /**
+   * @var PropertyRepository
+   */
+  private $repository;
+
+  /**
+   * @var ObjectManager
+   */
+  private $em;
+
+  /**
+   * PropertyController constructor.
+   * @param PropertyRepository $repository
+   */
+  public function __construct(PropertyRepository $repository, ObjectManager $em) {
+    $this->repository = $repository;
+    $this->em = $em;
+  }
+
+  /**
+   * @Route("/admin", name="admin.property.index")
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function index() {
+    $properties = $this->repository->findAll();
+    return $this->render('admin/property/index.html.twig', compact('properties'));
+  }
+
+  /**
+   * @param Property $property
+   * @param Request $request
+   * @Route("/admin/{id}", name="admin.property.edit")
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function edit(Property $property, Request $request) {
+    $form = $this->createForm(PropertyType::class, $property);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $this->em->flush();
+      return $this->redirectToRoute('admin.property.index');
+    }
+
+    return $this->render('admin/property/edit.html.twig', [
+      'property' => $property,
+      'form' => $form->createView()
+    ]);
+  }
+
+}
---
+++ src/Form/PropertyType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Property;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropertyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', null, [
+              'label' => 'Titre'
+            ])
+            ->add('description')
+            ->add('surface')
+            ->add('sold')
+            ->add('postalCode')
+            ->add('address')
+            ->add('price')
+            ->add('city')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Property::class,
+            'translation_domain' => 'forms'
+        ]);
+    }
+}
